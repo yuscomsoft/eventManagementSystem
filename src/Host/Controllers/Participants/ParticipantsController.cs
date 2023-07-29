56d07ac79ebffd6991358dd874b48d782779f74a
YusuffAhmad <yusuffahmad2005@gmail.com>
2023-07-29 20:03:59 +0100

    get participant by id and event id

--- src/Host/Controllers/Participants/ParticipantsController.cs
+++ src/Host/Controllers/Participants/ParticipantsController.cs
@@ -1,4 +1,5 @@
-﻿using EventManagment.Application.DTOs;
+﻿using EventManagment.Application.Catalog.Products;
+using EventManagment.Application.DTOs;
 using EventManagment.Application.Participants;
 using EventManagment.Domain.Events;
 using Microsoft.AspNetCore.Http;
@@ -9,11 +10,19 @@ namespace EventManagment.Host.Controllers.Participants;
 public class ParticipantsController : VersionNeutralApiController
 {
 
-    [HttpGet("{eventId:guid}")]
+    [HttpGet("{eventId:guid}/event")]
     [MustHavePermission(FSHAction.View, FSHResource.Participants)]
     [OpenApiOperation("Get a list of all participant by available filters.", "")]
     public async Task<PaginationResponse<ParticipantDto>> GetListAsync(Guid eventId, int? jamaatId, int? circuitId, ParticipantType? participantType)
     {
         return await Mediator.Send(new GetParticipantsByEventRequest(eventId, jamaatId, circuitId, participantType));
+    }
+
+    [HttpGet("{participantId:guid}")]
+    [MustHavePermission(FSHAction.View, FSHResource.Participants)]
+    [OpenApiOperation("Get participant details.", "")]
+    public async Task<ParticipantDto> GetAsync(Guid eventId, Guid participantId)
+    {
+        return await Mediator.Send(new GetParticipantByEventIdRequest(eventId, participantId));
     }
 }
