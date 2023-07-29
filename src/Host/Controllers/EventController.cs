994dce8d9e71c44984550f0a77e15d03c6f32e12
Adebayo0007 <tijaniadebayoabdllahi@gmail.com>
2023-07-29 18:48:45 +0100

    creation of event added

---
+++ src/Host/Controllers/EventController.cs
@@ -0,0 +1,17 @@
+﻿using EventManagment.Application.Catalog.Products;
+using EventManagment.Application.Events;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EventManagment.Host.Controllers;
+
+public class EventController : VersionedApiController
+{
+    [HttpPost]
+    [MustHavePermission(FSHAction.Create, FSHResource.Events)]
+    [OpenApiOperation("Create a new Event.", "")]
+    public Task<Guid> CreateAsync(CreateEventRequest request)
+    {
+        return Mediator.Send(request);
+    }
+}
