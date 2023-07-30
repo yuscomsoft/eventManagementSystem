956d6b8b0d1a58a926b38a35551ddd67c7b3320e
femaden <femadeen@outlook.com>
2023-07-30 03:40:20 -0500

    Update Comment , Search Comment by Event, Delete Comment Implimented

--- src/Core/Application/Comments/DeleteComment.cs
+++ src/Core/Application/Comments/DeleteComment.cs
@@ -1,12 +1,33 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using EventManagment.Domain.Common.Events;
+using EventManagment.Domain.Events;
 
-namespace EventManagment.Application.Comments
+namespace EventManagment.Application.Comments;
+public class DeleteCommentRequest : IRequest<Guid>
 {
-   public class DeleteComment
+    public Guid Id { get; set; }
+
+    public DeleteCommentRequest(Guid id) => Id = id;
+}
+
+public class DeleteCommentRequestHandler : IRequestHandler<DeleteCommentRequest, Guid>
+{
+    private readonly IRepository<Comment> _repository;
+    private readonly IStringLocalizer _t;
+
+    public DeleteCommentRequestHandler(IRepository<Comment> repository, IStringLocalizer<DeleteCommentRequestHandler> localizer) =>
+        (_repository, _t) = (repository, localizer);
+
+    public async Task<Guid> Handle(DeleteCommentRequest request, CancellationToken cancellationToken)
     {
+        var comment = await _repository.GetByIdAsync(request.Id, cancellationToken);
+
+        _ = comment ?? throw new NotFoundException(_t["Product {0} Not Found."]);
+
+        // Add Domain Events to be raised after the commit
+        comment.DomainEvents.Add(EntityDeletedEvent.WithEntity(comment));
+
+        await _repository.DeleteAsync(comment, cancellationToken);
+
+        return request.Id;
     }
 }
