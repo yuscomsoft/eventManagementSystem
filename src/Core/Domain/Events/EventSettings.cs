aad4cee9ae370fd8dadb27aebb90731162d7dcc3
YusuffAhmad <yusuffahmad2005@gmail.com>
2023-08-09 23:11:24 +0100

    Add adittional field to and event it in create

--- src/Core/Domain/Events/EventSettings.cs
+++ src/Core/Domain/Events/EventSettings.cs
@@ -1,17 +1,31 @@
 ﻿using EventManagment.Domain.Enums;
 
 namespace EventManagment.Domain.Events;
-public class EventSettings : AuditableEntity, IAggregateRoot
+public class EventSettings : AuditableEntity
 {
-    public Guid EventId { get; set; }
-    public string? QrCode { get; set; }
-    public string? ShortLink { get; set; }
-    public EventType EventType { get; set; } = default!;
-    public bool IsRegistrationActive { get; set; }
-    public DateTime? RegistrationStartDate { get; set; }
-    public DateTime? RegistrationEndDate { get; set; }
-    public DateTime? CheckInStartDate { get; set; }
-    public string DataSource { get; set; } = default!;
-    public bool IsPrivate { get; set; }
-    public Event Event { get; set; }
+    public Guid EventId { get; private set; }
+    public string? EventQrCode { get; private set; }
+    public string? ShortLink { get; private set; }
+    public EventType EventType { get; private set; } = default!;
+    public bool IsRegistrationActive { get; private set; }
+    public DateTime? RegistrationStartDate { get; private set; }
+    public DateTime? RegistrationEndDate { get; private set; }
+    public DateTime? CheckInStartDate { get; private set; }
+    public string DataSource { get; private set; } = default!;
+    public bool IsPrivate { get; private set; }
+    public Event Event { get; private set; }
+
+    public EventSettings(DefaultIdType eventId, string? eventQrCode, string? shortLink, EventType eventType, bool isRegistrationActive, DateTime? registrationStartDate, DateTime? registrationEndDate, DateTime? checkInStartDate, string dataSource, bool isPrivate)
+    {
+        EventId = eventId;
+        EventQrCode = eventQrCode;
+        ShortLink = shortLink;
+        EventType = eventType;
+        IsRegistrationActive = isRegistrationActive;
+        RegistrationStartDate = registrationStartDate;
+        RegistrationEndDate = registrationEndDate;
+        CheckInStartDate = checkInStartDate;
+        DataSource = dataSource;
+        IsPrivate = isPrivate;
+    }
 }
---
+++ src/Migrators/Migrators.MSSQL/Migrations/Application/20230809211912_AddToEventEntityForRequrementChanges.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Migrators.MSSQL.Migrations.Application
+{
+    /// <inheritdoc />
+    public partial class AddToEventEntityForRequrementChanges : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "EventRegistrationNumber",
+                schema: "Event",
+                table: "Participants",
+                newName: "TicketDownloadLink");
+
+            migrationBuilder.RenameColumn(
+                name: "QrCode",
+                schema: "Event",
+                table: "EventSettings",
+                newName: "EventQrCode");
+
+            migrationBuilder.AddColumn<string>(
+                name: "TickectReferenceNumber",
+                schema: "Event",
+                table: "Participants",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "TicketQrCode",
+                schema: "Event",
+                table: "Participants",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "RegistrationStartDate",
+                schema: "Event",
+                table: "EventSettings",
+                type: "datetime2",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "RegistrationEndDate",
+                schema: "Event",
+                table: "EventSettings",
+                type: "datetime2",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "CheckInStartDate",
+                schema: "Event",
+                table: "EventSettings",
+                type: "datetime2",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_EventSettings_EventId",
+                schema: "Event",
+                table: "EventSettings",
+                column: "EventId",
+                unique: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_EventSettings_Events_EventId",
+                schema: "Event",
+                table: "EventSettings",
+                column: "EventId",
+                principalSchema: "Event",
+                principalTable: "Events",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_EventSettings_Events_EventId",
+                schema: "Event",
+                table: "EventSettings");
+
+            migrationBuilder.DropIndex(
+                name: "IX_EventSettings_EventId",
+                schema: "Event",
+                table: "EventSettings");
+
+            migrationBuilder.DropColumn(
+                name: "TickectReferenceNumber",
+                schema: "Event",
+                table: "Participants");
+
+            migrationBuilder.DropColumn(
+                name: "TicketQrCode",
+                schema: "Event",
+                table: "Participants");
+
+            migrationBuilder.RenameColumn(
+                name: "TicketDownloadLink",
+                schema: "Event",
+                table: "Participants",
+                newName: "EventRegistrationNumber");
+
+            migrationBuilder.RenameColumn(
+                name: "EventQrCode",
+                schema: "Event",
+                table: "EventSettings",
+                newName: "QrCode");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "RegistrationStartDate",
+                schema: "Event",
+                table: "EventSettings",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified),
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "RegistrationEndDate",
+                schema: "Event",
+                table: "EventSettings",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified),
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "CheckInStartDate",
+                schema: "Event",
+                table: "EventSettings",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified),
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldNullable: true);
+        }
+    }
+}
