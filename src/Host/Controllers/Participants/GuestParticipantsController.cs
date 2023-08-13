15216dac8432af8fe9f0afa95423ee3c56670805
femaden <femadeen@outlook.com>
2023-08-13 18:38:44 -0500

    Update On registration By Guest

---
+++ src/Host/Controllers/Participants/GuestParticipantsController.cs
@@ -0,0 +1,20 @@
+﻿using EventManagment.Application.Catalog.Products;
+using EventManagment.Application.DTOs;
+using EventManagment.Application.Multitenancy;
+using EventManagment.Application.Participants;
+using EventManagment.Domain.Events;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EventManagment.Host.Controllers.Participants;
+
+public class GuestParticipantsController : VersionNeutralApiController
+{
+    [HttpPost]
+    [MustHavePermission(FSHAction.Create, FSHResource.Participants)]
+    [OpenApiOperation("Register Guest Participant.", "")]
+    public Task<Guid> CreateAsyn(CreateGuestParticipantRequest request)
+    {
+        return Mediator.Send(request);
+    }
+}
